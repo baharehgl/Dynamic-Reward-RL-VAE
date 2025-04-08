444b9719639c79f5eccf34c35b3ed9f1bdb87ce5
baharehgl <88573876+baharehgl@users.noreply.github.com>
2025-04-08 13:42:24 -0700

    Update myasp-smd.py

--- myasp-smd.py
+++ myasp-smd.py
@@ -29,31 +29,32 @@ from sklearn.svm import OneClassSVM
 from sklearn.semi_supervised import LabelPropagation, LabelSpreading
 from sklearn.metrics import precision_recall_fscore_support, average_precision_score
 
+# Set GPU devices if available.
 os.environ['CUDA_VISIBLE_DEVICES'] = "0,1"
 
 ############################
 # Macros and Hyperparameters.
-DATAFIXED = 0  # whether target is fixed to a single time series
-EPISODES = 300  # number of episodes (for demonstration)
-DISCOUNT_FACTOR = 0.5  # reward discount factor
-EPSILON = 0.5  # epsilon-greedy parameter
-EPSILON_DECAY = 1.00  # epsilon decay
+DATAFIXED = 0               # whether target is fixed to a single time series
+EPISODES = 300              # number of episodes (for demonstration)
+DISCOUNT_FACTOR = 0.5       # reward discount factor
+EPSILON = 0.5               # epsilon-greedy parameter
+EPSILON_DECAY = 1.00        # epsilon decay
 
 # Extrinsic reward values (heuristic):
-TN_Value = 1  # True Negative
-TP_Value = 5  # True Positive
-FP_Value = -1  # False Positive
-FN_Value = -5  # False Negative
+TN_Value = 1    # True Negative
+TP_Value = 5    # True Positive
+FP_Value = -1   # False Positive
+FN_Value = -5   # False Negative
 
+# Define action space.
 NOT_ANOMALY = 0
 ANOMALY = 1
 action_space = [NOT_ANOMALY, ANOMALY]
 action_space_n = len(action_space)
 
-n_steps = 25  # sliding window length
-n_input_dim = 2  # dimension of input to LSTM (value and action indicator)
-n_hidden_dim = 128  # hidden dimension
-
+n_steps = 25                # sliding window length
+n_input_dim = 2             # dimension of input to LSTM (value and action indicator)
+n_hidden_dim = 128          # hidden dimension
 validation_separate_ratio = 0.9
 
 ########################### VAE Setup #####################
@@ -96,6 +97,7 @@ def build_vae(original_dim, latent_dim=2, intermediate_dim=64):
     h = layers.Dense(intermediate_dim, activation='relu', kernel_initializer='he_normal')(h)
     z_mean = layers.Dense(latent_dim, kernel_initializer='he_normal')(h)
     z_log_var = layers.Dense(latent_dim, kernel_initializer='he_normal')(h)
+    # Clip log variance for numerical stability.
     z_log_var = tf.clip_by_value(z_log_var, -10.0, 10.0)
     z = Sampling()([z_mean, z_log_var])
     decoder_h = layers.Dense(intermediate_dim, activation='relu', kernel_initializer='he_normal')
@@ -112,7 +114,7 @@ def build_vae(original_dim, latent_dim=2, intermediate_dim=64):
     vae.compile(optimizer=optimizer)
     return vae, encoder
 
-original_dim = n_steps  # for univariate signal, original_dim equals n_steps
+original_dim = n_steps      # for univariate signal, original_dim equals n_steps
 latent_dim = 10
 intermediate_dim = 64
 
@@ -136,12 +138,18 @@ def RNNBinaryStateFuc(timeseries, timeseries_curser, previous_state=[], action=N
         return np.array([state0, state1], dtype='float32')
     return None
 
-def RNNBinaryRewardFuc(timeseries, timeseries_curser, action=0, vae=None, dynamic_coef=1.0):
+# New reward function option with flag to include VAE penalty.
+def RNNBinaryRewardFuc(timeseries, timeseries_curser, action=0, vae=None, dynamic_coef=1.0, include_vae_penalty=True):
     if timeseries_curser >= n_steps:
         current_state = np.array([timeseries['value'][timeseries_curser - n_steps:timeseries_curser]])
-        vae_reconstruction = vae.predict(current_state)
-        reconstruction_error = np.mean(np.square(vae_reconstruction - current_state))
-        vae_penalty = dynamic_coef * reconstruction_error
+        vae_penalty = 0.0
+        if include_vae_penalty and vae is not None:
+            # Compute reconstruction error from VAE.
+            vae_reconstruction = vae.predict(current_state)
+            reconstruction_error = np.mean(np.square(vae_reconstruction - current_state))
+            vae_penalty = dynamic_coef * reconstruction_error
+            # Log reconstruction error for diagnostics.
+            print("Reconstruction error: {:.5f}, VAE penalty: {:.5f}".format(reconstruction_error, vae_penalty))
         if timeseries['label'][timeseries_curser] == 0:
             return [TN_Value + vae_penalty, FP_Value + vae_penalty]
         elif timeseries['label'][timeseries_curser] == 1:
@@ -151,6 +159,7 @@ def RNNBinaryRewardFuc(timeseries, timeseries_curser, action=0, vae=None, dynami
     else:
         return [0, 0]
 
+# Testing reward function (without penalty).
 def RNNBinaryRewardFucTest(timeseries, timeseries_curser, action=0):
     if timeseries_curser >= n_steps:
         if timeseries['anomaly'][timeseries_curser] == 0:
@@ -170,6 +179,7 @@ class Q_Estimator_Nonlinear():
                                                   dtype=tf.float32, name="state")
             self.target = tf.compat.v1.placeholder(shape=[None, action_space_n],
                                                    dtype=tf.float32, name="target")
+            # Simple fully connected output from LSTM.
             self.weights = {'out': tf.Variable(tf.compat.v1.random_normal([n_hidden_dim, action_space_n]))}
             self.biases = {'out': tf.Variable(tf.compat.v1.random_normal([action_space_n]))}
             self.state_unstack = tf.compat.v1.unstack(self.state, n_steps, 1)
@@ -179,6 +189,7 @@ class Q_Estimator_Nonlinear():
             self.losses = tf.compat.v1.squared_difference(self.action_values, self.target)
             self.loss = tf.reduce_mean(self.losses)
             self.optimizer = tf.compat.v1.train.AdamOptimizer(learning_rate=learning_rate)
+            # Global step variable for learning rate scheduling and tracking.
             global_step_var = tf.compat.v1.get_collection(tf.compat.v1.GraphKeys.GLOBAL_VARIABLES, scope="global_step")[0]
             self.train_op = self.optimizer.minimize(self.loss, global_step=global_step_var)
             self.summaries = tf.compat.v1.summary.merge([
@@ -226,12 +237,13 @@ def make_epsilon_greedy_policy(estimator, nA, sess):
     return policy_fn
 
 # Proportional update for dynamic coefficient.
-def update_dynamic_coef_proportional(current_coef, episode_reward, target_reward=100.0, alpha=0.01, min_coef=0.1,
-                                     max_coef=10.0):
+def update_dynamic_coef_proportional(current_coef, episode_reward, target_reward=100.0, alpha=0.001, min_coef=0.1, max_coef=10.0):
     new_coef = current_coef + alpha * (target_reward - episode_reward)
-    return max(min(new_coef, max_coef), min_coef)
+    new_coef = max(min(new_coef, max_coef), min_coef)
+    print("Updating dynamic coefficient: old = {:.3f}, new = {:.3f}".format(current_coef, new_coef))
+    return new_coef
 
-# --- Active Learning class remains unchanged.
+# --- Active Learning class.
 class active_learning(object):
     def __init__(self, env, N, strategy, estimator, already_selected):
         self.env = env
@@ -294,7 +306,8 @@ def q_learning(env, sess, qlearn_estimator, target_estimator, num_episodes, num_
                replay_memory_size=500000, replay_memory_init_size=50000, experiment_dir='./log/',
                update_target_estimator_every=10000, discount_factor=0.99,
                epsilon_start=1.0, epsilon_end=0.1, epsilon_decay_steps=500000, batch_size=256,
-               num_LabelPropagation=20, num_active_learning=5, test=0, vae_model=None):
+               num_LabelPropagation=20, num_active_learning=5, test=0, vae_model=None,
+               include_vae_penalty=True):
     Transition = namedtuple("Transition", ["state", "reward", "next_state", "done"])
     replay_memory = []
     checkpoint_dir = os.path.join(experiment_dir, "checkpoints")
@@ -363,12 +376,13 @@ def q_learning(env, sess, qlearn_estimator, target_estimator, num_episodes, num_
         if len(replay_memory) >= replay_memory_init_size:
             break
 
-    dynamic_coef = 10.0
+    dynamic_coef = 10.0  # initial dynamic coefficient
     episode_rewards = []
     coef_history = []
 
     for i_episode in range(num_episodes):
-        env.rewardfnc = lambda ts, tc, a: RNNBinaryRewardFuc(ts, tc, a, vae_model, dynamic_coef=dynamic_coef)
+        # Use the new reward function with adjustable VAE penalty flag.
+        env.rewardfnc = lambda ts, tc, a: RNNBinaryRewardFuc(ts, tc, a, vae_model, dynamic_coef=dynamic_coef, include_vae_penalty=include_vae_penalty)
         episode_reward = 0.0
 
         if i_episode % 50 == 49:
@@ -378,6 +392,7 @@ def q_learning(env, sess, qlearn_estimator, target_estimator, num_episodes, num_
         per_loop_time1 = time.time()
         state = env.reset()
         env.states_list = [s for s in env.states_list if s is not None]
+        # Ensure testing environment is proper.
         while env.datasetidx > env.datasetrng * validation_separate_ratio:
             state = env.reset()
             env.states_list = [s for s in env.states_list if s is not None]
@@ -387,7 +402,7 @@ def q_learning(env, sess, qlearn_estimator, target_estimator, num_episodes, num_
         al = active_learning(env=env, N=num_active_learning, strategy='margin_sampling',
                              estimator=qlearn_estimator, already_selected=labeled_index)
         al_samples = al.get_samples()
-        print('labeling samples: ' + str(al_samples) + ' in env ' + str(env.datasetidx))
+        print('Labeling samples: ' + str(al_samples) + ' in env ' + str(env.datasetidx))
         labeled_index.extend(al_samples)
         num_label += len(al_samples)
         state_list = np.array(env.states_list).transpose(2, 0, 1)[0]
@@ -396,6 +411,7 @@ def q_learning(env, sess, qlearn_estimator, target_estimator, num_episodes, num_
         for new_sample in al_samples:
             label_list[new_sample] = env.timeseries['anomaly'][new_sample + n_steps]
             env.timeseries['label'][new_sample + n_steps] = env.timeseries['anomaly'][new_sample + n_steps]
+        # Process each labeled sample.
         for sample in labeled_index:
             if sample < len(env.states_list):
                 env.timeseries_curser = sample + n_steps
@@ -419,6 +435,8 @@ def q_learning(env, sess, qlearn_estimator, target_estimator, num_episodes, num_
             pseudo_label = lp_model.transduction_[index]
             env.timeseries['label'][index + n_steps] = pseudo_label
         per_loop_time2 = time.time()
+
+        # Update the Q-network.
         for i_epoch in range(num_epoches):
             if qlearn_estimator.summary_writer:
                 episode_summary = tf.compat.v1.Summary()
@@ -466,7 +484,7 @@ def q_learning_validator(env, estimator, num_episodes, record_dir=None, plot=1):
             state = env.reset()
             env.states_list = [s for s in env.states_list if s is not None]
             print('double reset')
-        print('testing on: ' + str(env.repodirext[env.datasetidx]))
+        print('Testing on: ' + str(env.repodirext[env.datasetidx]))
         predictions = []
         ground_truths = []
         ts_values = []
@@ -561,10 +579,12 @@ def train_wrapper(num_LP, num_AL, discount_factor):
     data_directory = os.path.join(current_dir, "SMD", "ServerMachineDataset", "train")
     x_train = load_normal_data(data_directory, n_steps)
     vae, _ = build_vae(original_dim, latent_dim, intermediate_dim)
+    # Increase epochs if necessary for better training.
     vae.fit(x_train, epochs=200, batch_size=32)
     vae.save('vae_model.h5')
     percentage = [1]
     test = 0
+    # You can try different percentages for early testing.
     for j in range(len(percentage)):
         exp_relative_dir = ['SMD_LP_1500init_warmup_h128_b256_300ep_num_LP' + str(num_LP) +
                             '_num_AL' + str(num_AL) + '_d' + str(discount_factor)]
@@ -576,7 +596,8 @@ def train_wrapper(num_LP, num_AL, discount_factor):
         for i in range(len(dataset_dir)):
             env = EnvTimeSeriesfromRepo(dataset_dir[i])
             env.statefnc = RNNBinaryStateFuc
-            env.rewardfnc = lambda ts, tc, a: RNNBinaryRewardFuc(ts, tc, a, vae, dynamic_coef=10.0)
+            # Use the reward function with VAE penalty for training.
+            env.rewardfnc = lambda ts, tc, a: RNNBinaryRewardFuc(ts, tc, a, vae, dynamic_coef=10.0, include_vae_penalty=True)
             env.timeseries_curser_init = n_steps
             env.datasetfix = DATAFIXED
             env.datasetidx = 0
@@ -613,7 +634,8 @@ def train_wrapper(num_LP, num_AL, discount_factor):
                                                            num_LabelPropagation=num_LP,
                                                            num_active_learning=num_AL,
                                                            test=test,
-                                                           vae_model=vae)
+                                                           vae_model=vae,
+                                                           include_vae_penalty=True)
                 final_metric, final_aupr = q_learning_validator(env_test, qlearn_estimator,
                                                     int(env.datasetsize * (1 - validation_separate_ratio)),
                                                     experiment_dir)
