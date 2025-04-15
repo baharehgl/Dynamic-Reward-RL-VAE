7c268aa725bf26d2694fab09141a37b55a1ae1e2
baharehgl <88573876+baharehgl@users.noreply.github.com>
2025-04-15 14:46:21 -0700

    Added WaDi dataset

--- test1.py
+++ test1.py
@@ -1,13 +1,18 @@
 import pandas as pd
 
-# Read the first CSV (WADI_14days_new.csv) and show first 20 rows
-df_14days = pd.read_csv("WADI_14days_new.csv")
+label_path = "WaDi/WADI.A2_19 Nov 2019/WADI_14days_new.csv"
+label_df = pd.read_csv(label_path, sep=",", header=None)
+
+
+df_14days = pd.read_csv(label_path, sep=",", header=None)
 print("First 20 rows of WADI_14days_new.csv:")
 print(df_14days.head(20))
 
 print("\n" + "="*80 + "\n")
 
 # Read the second CSV (WADI_attackdataLABLE.csv) and show first 20 rows
-df_attack_label = pd.read_csv("WADI_attackdataLABLE.csv")
+
+label_path2 = "WaDi/WADI.A2_19 Nov 2019/WADI_attackdataLABLE.csv"
+df_attack_label = pd.read_csv(label_path2, sep=",", header=None)
 print("First 20 rows of WADI_attackdataLABLE.csv:")
 print(df_attack_label.head(20))
