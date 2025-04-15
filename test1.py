d07acb3f15330f0f4b532d706d15e1e30c64bf8c
baharehgl <88573876+baharehgl@users.noreply.github.com>
2025-04-15 14:41:04 -0700

    Update test1.py

--- test1.py
+++ test1.py
@@ -1,6 +1,13 @@
 import pandas as pd
 
-label_path = "SMD/ServerMachineDataset/test/machine-1-1.txt"
-label_df = pd.read_csv(label_path, sep=",", header=None)
-print(label_df.shape)
-print(label_df.head(10))
+# Read the first CSV (WADI_14days_new.csv) and show first 20 rows
+df_14days = pd.read_csv("WADI_14days_new.csv")
+print("First 20 rows of WADI_14days_new.csv:")
+print(df_14days.head(20))
+
+print("\n" + "="*80 + "\n")
+
+# Read the second CSV (WADI_attackdataLABLE.csv) and show first 20 rows
+df_attack_label = pd.read_csv("WADI_attackdataLABLE.csv")
+print("First 20 rows of WADI_attackdataLABLE.csv:")
+print(df_attack_label.head(20))
